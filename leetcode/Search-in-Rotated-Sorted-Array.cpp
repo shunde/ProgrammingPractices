757267e4f11ccf44d676ba17cd6c80fd0dcf4885
c0d3r1991 <wustcsd@gmail.com>
2016-03-08 00:03:41 +0800

    TODO: solve it!

---
+++ leetcode/Search-in-Rotated-Sorted-Array.cpp
@@ -0,0 +1,9 @@
+class Solution {
+    int searchAux(vector<int>& nums, int left, int right, int target) {
+        
+    }
+public:
+    int search(vector<int>& nums, int target) {
+        
+    }
+};
