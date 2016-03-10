631c798e9e6d9e3ed02ad590bd0e25b8aac9e1b6
c0d3r1991 <wustcsd@gmail.com>
2016-03-11 00:14:01 +0800

    find nth Super Ugly Number

---
+++ leetcode/Super-Ugly-Number.hpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int nthSuperUglyNumber(int n, vector<int>& primes) {
+       int *uglyNums = new int[n];
+       int k = primes.size(); 
+       int *multiply = new int[k]();
+
+       uglyNums[0] = 1;
+       for (int i = 1; i < n; i++) {
+           int nextUgly = uglyNums[multiply[0]] * primes[0];
+           for (int j = 1; j < k; j++) {
+           	   nextUgly = min(nextUgly, uglyNums[multiply[j]] * primes[j]);
+           }
+
+           for (int j = 0; j < k; j++) {
+           	   if (nextUgly == uglyNums[multiply[j]] * primes[j])
+           	   	    multiply[j]++;
+           }
+           uglyNums[i] = nextUgly;
+       }
+       int ret = uglyNums[n-1];
+       delete uglyNums;
+       delete multiply;
+       return ret;
+    }
+};
