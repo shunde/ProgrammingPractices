8487c5a105b2c828f67825d028a65be811dcf9dc
c0d3r1991 <wustcsd@gmail.com>
2016-03-13 00:39:40 +0800

    implement yeild in c with switch

---
+++ coroutine/switch_version.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+int function(void) {
+  static int i, state = 0;
+  switch (state) {
+    case 0: /* start of function */
+    for (i = 0; i < 10; i++) {
+      state = 1; /* so we will come back to "case 1" */
+      return i;
+      case 1:; /* resume control straight after the return */
+    }
+  }
+}
+
+int main() {
+	for (int i = 0; i < 10; i++)
+		printf("%d\n", function());
+	return 0;
+}
