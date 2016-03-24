9951471a0303edbbbf0576f15d909278761cdcd4
shunde <wustcsd@gmail.com>
2016-03-24 20:03:26 +0800

    beware of overflow

---
+++ leetcode/Number-of-Digit-One.cc
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int countDigitOne(int n) {
+        if (n < 1) return 0;
+        int iCount = 0;
+        long long factor = 1;       // consider factor overflow, so use long long     
+        int iLowerNum = 0;
+        int iCurrentNum = 0;
+        int iHigherNum = 0;
+        while (n > factor) {
+            iLowerNum = n - (n / factor) * factor;
+            iCurrentNum = (n / factor) % 10;
+            iHigherNum = n / (factor * 10);
+
+            switch (iCurrentNum) {
+            case 0:
+                iCount += iHigherNum * factor;
+                break;
+            case 1:
+                iCount += iHigherNum * factor + iLowerNum + 1;
+                break;
+            default:
+                iCount += (iHigherNum + 1) * factor;
+                break;
+            }
+            factor *= 10;     // be aware of overflow.
+        }
+
+        return iCount;
+    }
+};
