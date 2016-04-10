18b4f0c577b9a79e3241b0f7e4458ebd538be57b
csd <wustcsd@gmail.com>
2016-04-11 00:13:32 +0800

    segment tree...todo...

---
+++ leetcode/range-sum-query-mutable.cc
@@ -0,0 +1,34 @@
+class NumArray {
+private:
+    struct node {
+        int left, right;
+        int sum;
+    };
+	
+public:
+    NumArray(vector<int> &nums) {
+        
+    }
+
+    void update(int i, int val) {
+        
+    }
+
+    int sumRange(int i, int j) {
+        
+    }
+private:
+    void build(int n) {
+
+
+    }
+private:
+    vector<node> segTree;
+};
+
+
+// Your NumArray object will be instantiated and called as such:
+// NumArray numArray(nums);
+// numArray.sumRange(0, 1);
+// numArray.update(1, 10);
+// numArray.sumRange(1, 2);
