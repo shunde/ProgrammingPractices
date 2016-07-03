4004fe5f8b29bb04ae7dfa9903b6964b2a827bc9
shunde <wustcsd@gmail.com>
2016-07-03 17:18:19 +0800

    Bit Manipulation

--- leetcode/sum-of-two-integers.cc
+++ leetcode/sum-of-two-integers.cc
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int getSum(int a, int b) {
-        int carry;
-        while (carry = a & b) {
+        while (b != 0) {
+            int carry = a & b;
             a = a ^ b;
             b = carry << 1;
         }
-        return a^b;
+        return a;
     }
 };
