88e2863da51c0139f7f5f4709cd12a77c02284ef
shunde <wustcsd@gmail.com>
2016-07-03 16:47:23 +0800

    another dp problem

--- leetcode/largest-divisible-subset.cc
+++ leetcode/largest-divisible-subset.cc
@@ -1,7 +1,15 @@
-// 最大团问题（极大完全子图）
 class Solution {
 public:
     vector<int> largestDivisibleSubset(vector<int>& nums) {
-        
+    	if (nums.size() < 2) return nums;
+    	sort(nums.begin(), nums.end());
+    	size_t n = nums.size();
+    	vector<int> dp(n, 1);
+    	vector<int> trace(n, -1);
+    	int largestSofar = 1;
+    	int largestIdxSofar = 0;
+    	vector<int> subset;
+    	
+    	return subset;        
     }
 };
