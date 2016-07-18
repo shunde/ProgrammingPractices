308c6a48c674a00c3275c895c13cf75d6ff0c0c6
csd <wustcsd@gmail.com>
2016-07-19 00:15:24 +0800

    相似问题和tag是很好的提示，类似剥洋葱一样，联想拓扑排序

--- leetcode/minimum-height-trees.cc
+++ leetcode/minimum-height-trees.cc
@@ -1,44 +1,47 @@
 class Solution {
-private:
-	class Graph {
-		vector<vector<int> > vertexs;
-		int n;
-	public:
-		Graph(int _n) {
-			n = _n;
-			vertexs = vector<vector<int> > (n, vector<int> ());
-		}
-		void addEdge(int v, int w) {
-			assert(v >= 0 && v < n);
-			assert(w >= 0 && w < n);
-
-			vertexs[v].push_back(w);
-			vertexs[w].push_back(v);
-		}
-		const vector<int>& getNeighbors(int v) {
-			assert(v >= 0 && v < n);
-			return vertexs[v];
-		}
-	}
-
-	int bfs(const Graph& graph, int v) {
-
-	}
 public:
 	vector<int> findMinHeightTrees(int n, vector<pair<int, int>>& edges) {
 		if (n == 0) return vector<int> ();
 		if (n == 1) return vector<int> {0};
 
-		vector<int> mht(n, -1);
-		Graph graph(n);
-		for (int i = 0; i < edges.size(); i++) {
-			graph.addEdge(edges[i].first, edges[i].second);
+		vector<vector<int> > graph(n, vector<int> ());
+		vector<int> degrees(n, 0);
+
+		for (auto e : edges) {
+			degrees[e.first]++;
+			degrees[e.second]++;
+
+			graph[e.first].push_back(e.second);
+			graph[e.second].push_back(e.first);
 		}
 
+		queue<int> workq;
 		for (int i = 0; i < n; i++) {
-			mht[i] = bfs(graph, i);
+			if (degrees[i] == 1) {
+				workq.push(i);
+			}
 		}
 
-		
+		while (n > 2) {
+			int sz = workq.size();
+			for (int i = 0; i < sz; i++)  {
+				int v = workq.front();
+				workq.pop();
+				n--;
+				for (auto w : graph[v]) {
+					degrees[w]--;
+					if (degrees[w] == 1)
+						workq.push(w);
+				}
+			}
+		}
+
+		vector<int> result;
+		while (!workq.empty()) {
+			result.push_back(workq.front());
+			workq.pop();
+		}
+
+		return result;
 	}
 };
