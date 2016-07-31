a9affc9de1f7321606dd754857db20fd51daeacb
shunde <wustcsd@gmail.com>
2016-07-31 16:53:13 +0800

    add trick-trick

---
+++ trick-trick/Readme.md
@@ -0,0 +1,6 @@
+Programming trick
+===
+
+- [Program that prints its own source code as its output](self-print-ii.c)
+
+
---
+++ trick-trick/self-print-ii.c
@@ -0,0 +1,2 @@
+#include <stdio.h>
+char*s="#include <stdio.h>%cchar*s=%c%s%c;main(){printf(s,10,34,s,34);}";main(){printf(s,10,34,s,34);}
