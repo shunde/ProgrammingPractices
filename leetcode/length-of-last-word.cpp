0f5ae9369dc0c9d7acf5cccd2c5f810ef71ee418
shunde <wustcsd@gmail.com>
2016-09-01 10:35:29 +0800

    acc

---
+++ leetcode/length-of-last-word.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+    	int len = s.length();
+    	int i, j;
+    	j = len - 1;
+    	while (j >= 0 && !isalpha(s[j])) j--;
+    	i = j-1;
+    	while (i >= 0 ** isalpha[s[i]]) i--;
+
+    	if (j >= 0) return j - i;
+    	else return 0;
+    }
+};
