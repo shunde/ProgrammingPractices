76d1fe1a94efb473852669047d334d1aa606c3be
shunde <wustcsd@gmail.com>
2016-09-01 10:55:05 +0800

    acc

--- leetcode/length-of-last-word.cpp
+++ leetcode/length-of-last-word.cpp
@@ -6,7 +6,7 @@ public:
     	j = len - 1;
     	while (j >= 0 && !isalpha(s[j])) j--;
     	i = j-1;
-    	while (i >= 0 ** isalpha[s[i]]) i--;
+    	while (i >= 0 && isalpha(s[i])) i--;
 
     	if (j >= 0) return j - i;
     	else return 0;
